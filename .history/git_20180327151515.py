f2ed05935c7d7ce0cd78d26b83527a09ab154d8a
Maciej Patej <maciejpatejmail@gmail.com>
2018-03-27 15:26:34 +0200

    THE TRUTH HAS BEEN SPOKEN

--- .history/git_20180327151515.py
+++ .history/git_20180327151515.py
@@ -1,15 +1,7 @@
 import time
 
-def shout():
-    print("BIG PHARMA IS KILLING US")
-
 def introduce():
     print("Hello, I'm Gittie!")
-
-
-def add():
-    print("Add me")
-
 
 def joke():
     print("Why did the chicken cross the road?")
